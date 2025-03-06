680559799527d2b532b26fed900f03f4886fe4a2
Maximilian Fenkart <maximilian.fenkart@sodex.at>
2025-03-06 21:58:48 +0100

    adding pyc to gitignore

--- hchtournament/hchtournament/mytournamentparser/my_tournament_parser.py
+++ hchtournament/hchtournament/mytournamentparser/my_tournament_parser.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 from .my_tournament_parser_base import MyTournamentParserBase
 from ..models import tournament_models
 
@@ -8,4 +10,6 @@ class MyTournamentParser(MyTournamentParserBase):
         super().__init__()
 
     def parse(self, fp_tournament: str) -> tournament_models.Tournament:
-        pass
+        
+        df = pd.read_excel(fp_tournament)
+        print(df)
